bdd0765893b19b6647e670f539849153cba51a9b
ir2010 <46022116+ir2010@users.noreply.github.com>
2020-02-15 00:17:02 +0530

    Splashscreen added

---
+++ app/src/main/java/com/ir_sj/litelo/FirstPage.java
@@ -0,0 +1,14 @@
+package com.ir_sj.litelo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
